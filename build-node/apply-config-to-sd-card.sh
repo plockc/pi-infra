688692762f15f2dbe432d1899dd79c39b9b635c1
Chris Plock <plockc@gmail.com>
2023-12-23 14:50:38 -0800

    found on build node

--- build-node/apply-config-to-sd-card.sh
+++ build-node/apply-config-to-sd-card.sh
@@ -8,7 +8,6 @@ PIROOT=/media/$USER/piroot
 pushd "$PIROOT"/etc/cloud/cloud.cfg.d
 sudo cp ~1/cloud-init-ssh.cfg 99-ssh.cfg
 sudo cp ~1/cloud-init-disable-network-config.cfg 99-disable-network-config.cfg
-sudo cp ~1/cloud-init-hostname.cfg 99-hostname.cfg
 popd
 
 pushd "$PIROOT"/etc
@@ -17,3 +16,7 @@ if [[ "${WIFI_SSID:-}" != "" ]]; then
 fi
 sudo cp ~1/sd-card-eth0.yaml netplan/
 popd
+
+if ! grep ifnames /media/$USER/piboot/cmdline.txt; then
+  sudo sed -i -e 's/$/ net.ifnames=0/' /media/$USER/piboot/cmdline.txt
+fi
